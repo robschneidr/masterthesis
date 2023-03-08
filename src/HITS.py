82a003d9ecf28f6b8690df162c640c5c72ca0c3a
robschneidr <48950292+robschneidr@users.noreply.github.com>
2023-03-08 13:53:44 +0100

    node ranking plots, similarity function

--- src/HITS.py
+++ src/HITS.py
@@ -8,6 +8,9 @@ Created on Fri Feb 24 17:06:25 2023
 import Graph
 import random
 import copy
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 
 '''
@@ -182,7 +185,28 @@ def HITS_one_step(graph, trust_included=False, trust_normalized=False):
         normalize_trusts(graph, sum_trusts)
         
 
-        
+def mean_nodes_order_similarity(nodeIDs_A, nodeIDs_B):
+     switched_order_A = [0 for _ in range(len(nodeIDs_A))]
+     switched_order_B = [0 for _ in range(len(nodeIDs_B))]
+     
+     print(nodeIDs_A)
+     print(nodeIDs_B)
+     
+     for i in range(len(nodeIDs_A)):
+         switched_order_A[nodeIDs_A[i]] = i
+         switched_order_B[nodeIDs_B[i]] = i
+         
+     print(switched_order_A)
+     print(switched_order_B)
+     
+     sum_differences = 0    
+     for A, B in zip(switched_order_A, switched_order_B):
+         sum_differences += abs(A - B)
+     sum_differences /= len(switched_order_A)
+     
+     return sum_differences
+     
+     
     
 
 
@@ -212,6 +236,62 @@ def sort_by_auth(node):
 def sort_by_hub(node):
     return node.hub
 
+def get_sorted_nodeIDs(params, sorted_nodes_auths, sorted_nodes_hubs, sorted_nodes_trusts):
+    
+    sorted_nodeIDs_auths = []
+    sorted_nodeIDs_hubs = []
+    sorted_nodeIDs_trusts = []
+    
+    for i in range(3):
+        if i == 0:
+            value_name = "auth"
+            vals = sorted_nodes_auths
+            sorted_nodeIDs = sorted_nodeIDs_auths
+            
+        elif i == 1:
+            value_name = "hub"
+            vals = sorted_nodes_hubs
+            sorted_nodeIDs = sorted_nodeIDs_hubs
+        else:
+            value_name = "trust"
+            vals = sorted_nodes_trusts
+            sorted_nodeIDs = sorted_nodeIDs_trusts
+                      
+        for i, param in enumerate(params):
+            sorted_nodeIDs.append([node._id for node in vals[i]])
+            print([node._id for node in vals[i]], "node " + value_name + " order " + param[name_id])
+            
+            
+            
+    return sorted_nodeIDs_auths, sorted_nodeIDs_hubs, sorted_nodeIDs_trusts
+
+def plot_node_rankings(params, sorted_nodeIDs_auths, sorted_nodeIDs_hubs, sorted_nodeIDs_trusts):
+    x = np.arange(0, len(sorted_nodeIDs_auths[0]))   
+    fig, axs = plt.subplots(3, 1)
+    for ID, param in zip(sorted_nodeIDs_auths, params): 
+        print(ID, param[name_id])
+        axs[0].plot(x, ID, label=param[name_id])
+
+    axs[0].set_ylabel('Authority')
+    #axs[0].grid(True)
+    
+    for ID, param in zip(sorted_nodeIDs_hubs, params): 
+        print(ID, param[name_id])
+        axs[1].plot(x, ID, label=param[name_id])
+    axs[1].set_ylabel('Hub')
+    #axs[1].grid(True)
+    
+    for ID, param in zip(sorted_nodeIDs_trusts, params): 
+        print(ID, param[name_id])
+        axs[2].plot(x, ID, label=param[name_id])
+    axs[2].set_xlabel('Node Ranking from Lowest to Highest')
+    axs[2].set_ylabel('Trust')
+    #axs[2].grid(True)
+    
+    fig.tight_layout()
+    plt.legend(loc='best', bbox_to_anchor=(1.2, 1))
+    plt.show()
+
 
 if __name__ == '__main__':
     
@@ -240,8 +320,8 @@ if __name__ == '__main__':
     
     
     params.append(std_HITS_param)
-    #params.append(std_HITS_rndHubAuth_param)
-    #params.append(trust_HITS_normalized)
+    params.append(std_HITS_rndHubAuth_param)
+    params.append(trust_HITS_normalized)
     params.append(rndTrust_rndHits_normalized)
     params.append(rnd_trust_HITS_normalized)
     #params.append(trust_HITS_avg)
@@ -263,23 +343,14 @@ if __name__ == '__main__':
         sorted_nodes_hubs.append(sorted_nodes_hub)
         sorted_nodes_trusts.append(sorted_nodes_trust)
         
+      
+    
+    sorted_nodeIDs_auths, sorted_nodeIDs_hubs, sorted_nodeIDs_trusts = get_sorted_nodeIDs(params, sorted_nodes_auths, sorted_nodes_hubs, sorted_nodes_trusts)    
+    
+    plot_node_rankings(params, sorted_nodeIDs_auths, sorted_nodeIDs_hubs, sorted_nodeIDs_trusts)    
         
-    for i in range(3):
-        if i == 0:
-            value_name = "auth"
-            vals = sorted_nodes_auths
-        elif i == 1:
-            value_name = "hub"
-            vals = sorted_nodes_hubs
-        else:
-            value_name = "trust"
-            vals = sorted_nodes_trusts
-                      
-        for i, param in enumerate(params):
-            print([node._id for node in vals[i]], "node " + value_name + " order " + param[name_id])
-            
-        print()
-        
+    diff = mean_nodes_order_similarity([node._id for node in sorted_nodes_auths[0]], [node._id for node in sorted_nodes_auths[1]])
+    print(diff)    
         
         
     
